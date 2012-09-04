03d584ef8159906a3c757e7f2514b1efd08ffd37
Daniel Puglisi <pulleasy@gmail.com>
2012-09-04 16:36:28 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # Creating directory
+echo "Creating directory: /etc/cron.custom"
 mkdir -p /etc/cron.custom
+
 # Downloading keep-alive.sh and save it into /etc/cron.custom directory
+echo "Downloading file 'keep-alive.sh' into /etc/cron.custom"
 curl --silent --output /etc/cron.custom/keep-alive.sh https://raw.github.com/danielpuglisi/keep-alive/master/keep-alive.sh
+
 # Add cronjob to the crontab file
-curl https://raw.github.com/danielpuglisi/keep-alive/master/crontab >> /etc/crontab
+echo "Adding cronjob to /etc/crontab"
+curl --silent https://raw.github.com/danielpuglisi/keep-alive/master/crontab >> /etc/crontab
+
+echo
+echo "Done"
